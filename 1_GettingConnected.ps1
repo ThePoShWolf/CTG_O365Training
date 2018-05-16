6fa48269cfe7dcc68dd0208fe7e8c2f5a27a76d1
Anthony Howell <anthony@howell-it.com>
2018-05-16 11:44:21 -0700

    Some minor updates

--- 1_GettingConnected.ps1
+++ 1_GettingConnected.ps1
@@ -1,28 +1,38 @@
+#region demo
 Throw "This is a demo, not a script."
 <#
     https://docs.microsoft.com/en-us/office365/enterprise/powershell/connect-to-all-office-365-services-in-a-single-windows-powershell-window
 #>
+#endregion
 
 #region Prereqs
 
-#required modules
+#region required modules
 #Earlier versions of Windows: WMF 5.1
 https://www.microsoft.com/en-us/download/details.aspx?id=54616
+
 #AzureAD
 Install-Module -Name AzureAD
 Update-Module -Name AzureAD
+
 #SharePoint Online
 https://www.microsoft.com/en-us/download/details.aspx?id=35588
+
 #Skype for Business Online
 https://www.microsoft.com/en-us/download/details.aspx?id=39366
+
 #Teams
 Install-Module -Name MicrosoftTeams
 Update-Module -Name MicrosoftTeams
+
 #Exchange
 #Nothing needed
+#endregion
 
-#Windows PowerShell needs to be configured to run signed scripts for Skype for Business Online, Exchange Online, and the Security & Compliance Center.
-#To do this, run the following command in an elevated Windows PowerShell session (a Windows PowerShell window you open by selecting Run as administrator).
+<#
+Windows PowerShell needs to be configured to run signed scripts for Skype for Business Online,
+Exchange Online, and the Security & Compliance Center.
+#>
 
 Set-ExecutionPolicy RemoteSigned
 
@@ -31,8 +41,10 @@ Set-ExecutionPolicy RemoteSigned
 #region Getting Connected
 
 $domainHost = "howlit"
+
 #Admin credentials for O365
 $credential = Get-Credential
+
 #Connect to AzureAD using the $credential
 Connect-AzureAD -Credential $credential
 
