925f7a4a385639668fb31415a76b6fa4752fc69c
Anthony Howell <anthony@howell-it.com>
2018-05-17 16:04:05 -0700

    Small adjustments and clarification

--- 2_CreatingAccounts.ps1
+++ 2_CreatingAccounts.ps1
@@ -2,6 +2,23 @@
 Throw 'This is a demo, dummy!'
 #endregion
 
+#region connection
+#Admin credentials for O365
+$credential = Get-Credential
+
+Connect-AzureAD -Credential $credential
+
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession
+
+#endregion connection
+
+#region prep
+Function Prompt(){}
+Clear-Host
+Remove-AzureADUser -ObjectID Rick.Sanchez@howell-it.com
+#endregion
+
 #region create user
 
 #connect to AzureAD
@@ -37,18 +54,19 @@ Get-AzureADUser -ObjectId $user.UserPrincipalName | Format-Table AssignedLicense
 
 #region assign licenses
 #create license objects
-$license = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
 $licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+$license = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
 
 #retrieve the correct SKU ID
+Get-AzureADSubscribedSku
 $license.skuid = (Get-AzureADSubscribedSku | Where-Object SKUPartNumber -eq "SMB_Business_Premium").SkuId
+$license.SkuId
 $licenses.AddLicenses = $license
 
 #assign the license
 Set-AzureADUserLicense -ObjectId $user.UserPrincipalName -AssignedLicenses $licenses
 
 #verify
-(Get-AzureADUser -ObjectId $user.UserPrincipalName).AssignedLicenses
-$license.SkuId
+Get-AzureADUser -ObjectId $user.UserPrincipalName | Format-List AssignedLicenses
 
 #endregion
