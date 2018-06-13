dbda1b790abff72e45e560bd52feb5415e6b057d
Anthony Howell <anthony@howell-it.com>
2018-06-13 16:40:07 -0700

    Minor edits for clarification

--- 2_CreatingAccounts.ps1
+++ 2_CreatingAccounts.ps1
@@ -5,13 +5,15 @@ Throw 'This is a demo, dummy!'
 #region prep
 Function Prompt(){}
 Clear-Host
-Remove-AzureADUser -ObjectID Rick.Sanchez@howell-it.com
+Remove-AzureADUser -ObjectID Rick.Sanchez@office365courses.com
 #endregion
 
 #region connection
+
 #Admin credentials for O365
 $credential = Get-Credential
 
+#In case we're not connected from part 1:
 Connect-AzureAD -Credential $credential
 
 #endregion connection
@@ -21,6 +23,8 @@ Connect-AzureAD -Credential $credential
 #create the password profile
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 $PasswordProfile.Password = "bqQr5b9HT"
+
+#These default to true, but to demonstrate:
 $PasswordProfile.EnforceChangePasswordPolicy = $true
 $PasswordProfile.ForceChangePasswordNextLogin = $true
 
@@ -34,32 +38,37 @@ $user = @{
     'UsageLocation' = 'US'
 }
 
+#region no splat
+New-AzureADUser -PasswordProfile $PasswordProfile -AccountEnabled $true -UserPrincipalName 'Rick.Sanchez@office365courses.com' -DisplayName 'Rick Sanchez' -MailNickName = 'RickSanchez' -UsageLocation = 'US'
+#endregion
+
 #create the user
 New-AzureADUser @User
 
 #verify
-Get-AzureADUser -ObjectId $user.UserPrincipalName
+Get-AzureADUser -ObjectId 'Rick.Sanchez@office365courses.com'
 
 #check licenses
-Get-AzureADUser -ObjectId $user.UserPrincipalName | Format-Table AssignedLicenses
-#Remove-AzureADUser -ObjectId $user.UserPrincipalName
+Get-AzureADUser -ObjectId 'Rick.Sanchez@office365courses.com' | Select-Object -ExpandProperty AssignedLicenses
+
 #endregion
 
 #region assign licenses
+
 #create license objects
 $licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
 $license = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
 
 #retrieve the correct SKU ID
 Get-AzureADSubscribedSku
-$license.skuid = (Get-AzureADSubscribedSku | Where-Object SKUPartNumber -eq "O365_Business_Premium").SkuId
+$license.skuid = (Get-AzureADSubscribedSku | Where-Object SKUPartNumber -eq "EXCHANGESTANDARD").SkuId
 $license.SkuId
 $licenses.AddLicenses = $license
 
 #assign the license
-Set-AzureADUserLicense -ObjectId $user.UserPrincipalName -AssignedLicenses $licenses
+Set-AzureADUserLicense -ObjectId 'Rick.Sanchez@office365courses.com' -AssignedLicenses $licenses
 
 #verify
-Get-AzureADUser -ObjectId $user.UserPrincipalName | Format-List AssignedLicenses
+Get-AzureADUser -ObjectId 'Rick.Sanchez@office365courses.com' | Select-Object -ExpandProperty AssignedLicenses
 
 #endregion
