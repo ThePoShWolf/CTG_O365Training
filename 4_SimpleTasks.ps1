5814323398b30986a316f76f961b55bc8b647621
Anthony Howell <anthony@howell-it.com>
2018-06-13 16:40:30 -0700

    Added content

---
+++ 4_SimpleTasks.ps1
@@ -0,0 +1,21 @@
+#region demo
+Throw 'This is a demo, dummy!'
+#endregion
+
+#region prep
+Function Prompt(){}
+Clear-Host
+#endregion
+
+#region Reset a user password
+
+#endregion
+
+#region Creating groups
+
+#endregion 
+
+#region Add and removing user from/to groups
+
+#endregion
+
