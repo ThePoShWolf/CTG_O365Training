3367f79bf0d6f0c6420e3ebd1bcdcd76293dfee2
Anthony Howell <anthony@howell-it.com>
2018-06-13 16:40:26 -0700

    Minor edits for clarification, added the connection section

--- 3_ManagingUsersMailboxes.ps1
+++ 3_ManagingUsersMailboxes.ps1
@@ -2,11 +2,37 @@
 Throw 'This is a demo, dummy!'
 #endregion
 
+#region prep
+Get-Mailbox | Set-CASMailbox -PopEnabled $true -ImapEnabled $true
+Set-MailboxAutoReplyConfiguration -Identity rick.sanchez@office365courses.com -ExternalMessage '' -InternalMessage ''
+Remove-Mailbox 'Garage@office365courses.com'
+Function Prompt(){}
+Clear-Host
+#endregion
+
+#region connection
+
+#In case we're not connected from part 1:
+$credential = Get-Credential
+
+$ExchangeSessionParams = @{
+    'ConfigurationName' = 'Microsoft.Exchange'
+    'ConnectionUri' = "https://outlook.office365.com/powershell-liveid/"
+    'Credential' = $credential
+    'Authentication' = 'Basic'
+    'AllowRedirection' = $true
+}
+$exchangeSession = New-PSSession  @ExchangeSessionParams
+Import-PSSession $exchangeSession -AllowClobber
+
+#endregion
 
 #region Mailbox Protocols
 Get-CASMailbox -Identity rick.sanchez@office365courses.com
 
-Set-CASMailbox -Identity -PopEnabled $false -ImapEnabled $false
+Set-CASMailbox -Identity rick.sanchez@office365courses.com -PopEnabled $false -ImapEnabled $false
+
+Get-CASMailbox -Identity rick.sanchez@office365courses.com
 
 Get-Mailbox | Set-CASMailbox -PopEnabled $false -ImapEnabled $false
 #endregion
@@ -16,27 +42,27 @@ Get-MailboxAutoReplyConfiguration -Identity rick.sanchez@office365courses.com
 
 $AutoReply = @{
     'Identity' = 'Rick.Sanchez@office365courses.com'
-    'AutoReplyState' = 'Scheduled'
-    'StartTime' = '12/20/2018 17:00:00'
-    'EndTime' = '01/02/19 08:00:00'
+    'AutoReplyState' = 'Enabled'
+    'ExternalMessage' = "I'll be out for a while with the grandkids, back on the 2nd!"
+    'InternalMessage' = "I'll be out for a while with the grandkids, back on the 2nd!"
 }
 
 Set-MailboxAutoReplyConfiguration @AutoReply
 
-Get-MailboxAutoReplyConfiguration -Identity $AutoReply.Identity
+Get-MailboxAutoReplyConfiguration -Identity 'Rick.Sanchez@office365courses.com'
 
 $AutoReply = @{
     'Identity' = 'Rick.Sanchez@office365courses.com'
     'AutoReplyState' = 'Scheduled'
     'StartTime' = '12/20/2018 17:00:00'
     'EndTime' = '01/02/19 08:00:00'
-    'ExternalAudience' = 'All'
+    'ExternalAudience' = 'All' # Known | None
     'InternalMessage' = "<html><body>Thanks for reaching out, I'm enjoying Christmas with my grandkids.<br><br>Wubba lubba dub dub!<br>-Rick</body></html>"
     'ExternalMessage' = "<html><body>Thanks for reaching out, I'm enjoying Christmas with my grandkids.<br><br>Wubba lubba dub dub!<br>-Rick</body></html>"
 }
 Set-MailboxAutoReplyConfiguration @AutoReply
 
-Get-MailboxAutoReplyConfiguration $AutoReply.Identity
+Get-MailboxAutoReplyConfiguration 'Rick.Sanchez@office365courses.com'
 
 #endregion
 
@@ -44,22 +70,23 @@ Get-MailboxAutoReplyConfiguration $AutoReply.Identity
 $ResourceMailbox = @{
     'Room' = $true
     'Name' = 'Garage'
-    #'DisplayName' = 'Garage'
 }
 New-Mailbox @ResourceMailbox
+
+New-Mailbox -Name 'Garage' -Room
 
 $CalendarProcessing = @{
     'Identity' = 'Garage@office365courses.com'
     'AllowConflicts' = $false
     'AllowRecurringMeetings' = $true
-    'AutomateProcessing' = 'AutoAccept'
+    'AutomateProcessing' = 'AutoAccept' #None | AutoUpdate
     'BookingWindowInDays' = 365
     'MaximumDurationInMinutes' = 600
     'ProcessExternalMeetingMessages' = $false
 }
 Set-CalendarProcessing @CalendarProcessing
 
-Get-CalendarProcessing $CalendarProcessing.Identity
+Get-CalendarProcessing 'Garage@office365courses.com'
 
 #endregion
 
@@ -81,6 +108,10 @@ Set-MobileDevice @MobileDevice
 #endregion
 
 #region mailbox permissions
+Get-MailboxPermission 'Rick.Sanchez@office365courses.com'
+
+Get-MailboxPermission 'Rick.Sanchez@office365courses.com' | Where-Object IsInherited -eq $false
+
 $MailboxPermission = @{
     'Identity' = 'Rick.Sanchez@office365courses.com'
     'User' = 'admin@office365courses.com'
@@ -89,10 +120,10 @@ $MailboxPermission = @{
 }
 Add-MailboxPermission @MailboxPermission
 
-Get-MailboxPermission $MailboxPermission.Identity | Where-Object User -like "*admin*"
+Get-MailboxPermission 'Rick.Sanchez@office365courses.com' | Where-Object IsInherited -eq $false
 
 Remove-MailboxPermission @MailboxPermission
 
-Get-MailboxPermission $MailboxPermission.Identity | Where-Object User -like "*admin*"
+Get-MailboxPermission 'Rick.Sanchez@office365courses.com' | Where-Object IsInherited -eq $false
 
 #endregion
