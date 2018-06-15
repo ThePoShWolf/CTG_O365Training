3efaf51ed6e613181f8956148c4ab0d319697879
Anthony Howell <anthony@howell-it.com>
2018-06-15 12:18:23 -0700

    splatted and organized

--- 4_SimpleTasks.ps1
+++ 4_SimpleTasks.ps1
@@ -12,7 +12,7 @@ Clear-Host
 #region connect
 $credential = Get-Credential
 Connect-AzureAD -Credential $credential
-#
+#endregion
 
 #region Reset a user password
 $passwordChange = @{
@@ -29,20 +29,42 @@ Set-AzureADUserPassword -ObjectId 'rick.sanchez@office365courses.com' -Password 
 #endregion
 
 #region Creating groups
+
 #security enabled group
+$SecurityEnabled = @{
+    'DisplayName' = 'Underground Lair Admins'
+    'SecurityEnabled' = $true
+    'MailEnabled' = $false
+    'MailNickName' = 'null'
+}
+New-AzureADGroup @SecurityEnabled
+
+#No Splat
 New-AzureADGroup -DisplayName 'Underground Lair Admins' -SecurityEnabled $true -MailEnabled $false -MailNickName 'null'
 
 #mail enabled group
+$MailEnabled = @{
+    'DisplayName' = 'Smartest Beings'
+    'SecurityEnabled' = $false
+    'MailEnabled' = $true
+    'MailNickName' = 'SmartestBeings'
+}
+New-AzureADGroup @MailEnabled
+
+#No Splat
 New-AzureADGroup -DisplayName 'Smartest Beings' -SecurityEnabled $false -MailEnabled $true -MailNickName 'SmartestBeings'
 
 #endregion 
 
 #region Add and removing user from/to groups
+#retrieve appropriate ObjectIds
 $ULA = Get-AzureADGroup -Filter "DisplayName eq 'Underground Lair Admins'"
 $Rick = Get-AzureADUser -ObjectId 'Rick.Sanchez@office365courses.com'
 
+#Add to a group
 Add-AzureADGroupMember -ObjectId $ULA.ObjectID -RefObjectId $Rick.ObjectId
 
+#Remove from the same group
 Remove-AzureADGroupMember -ObjectId $ULA.ObjectId -MemberId $Rick.ObjectId
 
 #endregion
